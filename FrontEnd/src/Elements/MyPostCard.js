d933e368c274a7262e042858d31716c26f3daf18
sriniwas14 <sriniwasx@gmail.com>
2021-04-06 14:59:08 +0530

    Implemented Save in the FrontEnd

--- FrontEnd/src/Elements/MyPostCard.js
+++ FrontEnd/src/Elements/MyPostCard.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 export default function MyPostCard({ post }) {
     return (
-        <div>
-            <Link to={`/myposts/${post._id}`}>
-            { post.title }</Link>
+        <Link className="csMyPostLink" to={`/myposts/${post._id}`}>
+        <div className="csMyPostCard">
+                <Container>
+                    <Row>
+                        <Col sm={2}>
+                            <img style={{ width: "100%" }} src={`${process.env.REACT_APP_API_URL}/uploads/featured/default.jpg`} />    
+                        </Col>    
+                        <Col className="csMPCDetails" sm={8}>
+                            <h3>{ post.title }</h3>
+                            <p>{ post.excerpt }</p>
+                            <p className="csPostDate">Published {moment(post.postedOn).format('LL')}</p>
+                        </Col>
+                    </Row>
+                </Container>
         </div>
+        </Link>
     )
 }
--- FrontEnd/src/Pages/EditPost.js
+++ FrontEnd/src/Pages/EditPost.js
@@ -1,9 +1,129 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
+import ImageUploader from "react-images-upload";
+import { useParams } from "react-router-dom";
+import axiosInstance from "../utils/Api";
+import { useData } from "../DataContext";
 
 export default function EditPost() {
-    return (
-        <div>
-            Add Post
-        </div>
-    )
+  const { posts } = useData();
+  const [currentPost, setCurrentPost] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [pictures, setPictures] = useState(null);
+
+  let { postId } = useParams();
+
+  useEffect(() => {
+    const post = posts.filter((p) => p._id === postId);
+    setCurrentPost(post[0]);
+  }, []);
+
+  const handleChange = (e) => {
+    setCurrentPost((post) => ({ ...post, [e.target.name]: e.target.value }));
+  };
+
+  const handleSave = () => {
+    setLoading(true);
+    axiosInstance
+      .put(`/posts`, {
+        ...currentPost,
+      })
+      .then((response) => {
+        if (!response.data.success) return;
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log("Err ", err);
+      });
+  };
+
+  const onDrop = (picture) => {
+    const formData = new FormData();
+    formData.append("featured", picture[0]);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+    axiosInstance
+      .post(`/posts/${postId}/upload`, formData, config)
+      .then((response) => {
+        alert("The file is successfully uploaded");
+      })
+      .catch((error) => {});
+
+    setPictures(picture);
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col sm={6}>
+          <h2>Edit Post</h2>
+        </Col>
+        <Col style={{ textAlign: "right" }} sm={6}>
+          {loading && (
+            <div style={{ padding: 10, display: "inline" }}>
+              <Spinner style={{ height: 21, width: 21 }} animation="border" />
+            </div>
+          )}
+          <button className="csButton csPostsButton" onClick={handleSave}>Save</button>
+          <button
+            style={{ marginLeft: 5, backgroundColor: "#ff4734" }}
+            onClick={() => window.history.back()}
+            className="csButton csPostsButton"
+          >
+            Quit
+          </button>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col sm={12}>
+          <h3>Title</h3>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="title"
+            value={currentPost?.title}
+            placeholder="Title"
+            className="csInput"
+          />
+        </Col>
+        <Col sm={12}>
+          <h3>Excerpt</h3>
+          <textarea
+            onChange={handleChange}
+            name="excerpt"
+            value={currentPost?.excerpt}
+            placeholder="Excerpt"
+            className="csInput"
+          />
+        </Col>
+        <Col sm={12}>
+          <h3>Featured Image</h3>
+          <ImageUploader
+            withPreview={true}
+            singleImage={true}
+            withIcon={true}
+            onChange={onDrop}
+            imgExtension={[".jpg"]}
+            maxFileSize={5242880}
+          />
+        </Col>
+        <Col sm={12}>
+          <h3>Content</h3>
+          <textarea
+            onChange={handleChange}
+            name="content"
+            value={currentPost?.content}
+            rows="15"
+            placeholder="Content"
+            className="csInput"
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
