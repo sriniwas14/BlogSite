500e13a83217b58438bdfa194e94f5efcaa91144
sriniwas14 <sriniwasx@gmail.com>
2021-04-06 16:01:31 +0530

    Created SinglePost Component

--- FrontEnd/src/Pages/SinglePost.js
+++ FrontEnd/src/Pages/SinglePost.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import { useParams } from "react-router-dom";
+import moment from 'moment'
+
+import { useData } from "../DataContext";
+import PostCard from '../Elements/PostCard';
 
 export default function SinglePost() {
+    const { posts } = useData();
+    const [currentPost, setCurrentPost] = useState({});
+
+    const { postId } = useParams();
+
+    useEffect(() => {
+        const post = posts.filter((p) => p._id === postId);
+        setCurrentPost(post[0]);
+    }, []);
+
+      
     return (
         <div>
-            Single Post
+            <Container className="csSinglePostPage">
+                <Row>
+                    <Col sm={8}>
+                        <img src={`${process.env.REACT_APP_API_URL}/uploads/featured/default.jpg`} />
+                        <p className="csPostDate">Published {moment(currentPost.postedOn).format('LL')}</p>
+                        <h2>{currentPost?.title}</h2>
+                        <p>{currentPost?.content}</p>
+                    </Col>
+                    <Col sm={4}>
+                        {
+                            posts.map(post => <PostCard post={post} fullWidth={false } />)    
+                        }
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
