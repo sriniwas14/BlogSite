29c2a348c300d186df7632f73f6de9f806daa314
sriniwas14 <sriniwasx@gmail.com>
2021-04-18 10:47:51 +0530

    Now Loading Post from the BackEnd

--- FrontEnd/src/Pages/SinglePost.js
+++ FrontEnd/src/Pages/SinglePost.js
@@ -2,20 +2,44 @@ import React, { useState, useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import { useParams } from "react-router-dom";
 import moment from 'moment'
+import axiosInstance from "../utils/Api";
 
-import { useData } from "../DataContext";
+import { useData, useDataUpdate } from "../DataContext";
 import PostCard from '../Elements/PostCard';
 
 export default function SinglePost() {
     const { posts } = useData();
+    const { setPosts } = useDataUpdate();
     const [currentPost, setCurrentPost] = useState({});
 
     const { postId } = useParams();
 
+    // Load Post from the BackEnd
     useEffect(() => {
-        const post = posts.filter((p) => p._id === postId);
-        setCurrentPost(post[0]);
+        if(posts.length>0) return
+        axiosInstance.get(`/posts`, {})
+        .then((response) => {
+            if (!response.data.success) return 
+            setPosts(response.data.data)
+        })
+        .catch(err => {
+            console.log("Err ", err)
+        })
+    }, [])
+
+    // Load Post from Posts Array
+    useEffect(() => {
+        console.log("A")
+        if(posts.length===0) return
+
+        console.log("B", getCurrentPost(postId))
+        setCurrentPost(getCurrentPost(postId))
     }, []);
+
+    const getCurrentPost = (currentPostId) => {
+        const post = posts.filter((p) => p._id === currentPostId);
+        return post[0]
+    }
 
       
     return (
@@ -23,8 +47,8 @@ export default function SinglePost() {
             <Container className="csSinglePostPage">
                 <Row>
                     <Col sm={8}>
-                        <img src={`${process.env.REACT_APP_API_URL}${currentPost.featuredImage}`} />
-                        <p className="csPostDate">Published {moment(currentPost.postedOn).format('LL')}</p>
+                        <img src={`${process.env.REACT_APP_API_URL}${currentPost?.featuredImage}`} />
+                        <p className="csPostDate">Published {moment(currentPost?.postedOn).format('LL')}</p>
                         <h2>{currentPost?.title}</h2>
                         <p>{currentPost?.content}</p>
                     </Col>
